7f3a022102978e429e8a793a40de81095fff268e
tazman230a <76573669+tazman230a@users.noreply.github.com>
2025-01-17 16:00:34 -0800

    first commit

---
+++ components/Social.tsx
@@ -0,0 +1,30 @@
+
+import Link from "next/link";
+import { FaGithub, FaLinkedinIn, FaYoutube, FaTwitter } from "react-icons/fa"
+
+const socials = [
+    {icon: <FaGithub />, path: ""},
+    {icon: <FaLinkedinIn />, path: ""},
+    {icon: <FaYoutube />, path: ""},
+    {icon: <FaTwitter />, path: ""},
+]
+
+const Social = ({ containerStyles, iconsStyles}) => {
+  return (
+    <div className={containerStyles}> 
+      {socials.map((item, index) => {
+        return (
+            <Link 
+                key={index}
+                href={item.path}
+                className={iconsStyles}    
+            >
+              {item.icon}
+            </Link>
+        )
+      })}
+    </div>
+  )
+};
+
+export default Social;
